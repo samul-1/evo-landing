203d5890da4c92921ce1af4603cffa1b3ff2615a
Samuele <bsamuele00@gmail.com>
2022-09-07 18:24:20 +0200

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+function animateValues() {
+  animateValue(document.getElementById("number-1"), 100, 800, 1500);
+  animateValue(document.getElementById("number-2"), 1000, 1700, 1500);
+  animateValue(document.getElementById("number-3"), 1000, 8500, 1500);
+  animateValue(document.getElementById("number-4"), 10, 60, 1500);
+}
+
+const options = {
+  root: null,
+  threshold: 0.25, // 0 - 1 this work as a trigger.
+  rootMargin: "0px",
+};
+
+const target = document.querySelector("#numbers-section");
+const observer = new IntersectionObserver((entries) => {
+  console.log("INTERS", entries);
+  // each entry checks if the element is the view or not and if yes trigger the function accordingly
+  entries.filter((e) => e.isIntersecting).forEach(() => animateValues());
+}, options);
+observer.observe(target);
