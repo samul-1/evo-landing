935f64510f68fc236e415a792ba9f8559e563095
Samuele <bsamuele00@gmail.com>
2022-10-01 00:01:09 +0200

    added try button & updated numbers

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function animateValues() {
   if (animated) {
     return;
   }
-  animateValue(document.getElementById("number-1"), 100, 800, 1500);
-  animateValue(document.getElementById("number-2"), 1000, 1700, 1500);
-  animateValue(document.getElementById("number-3"), 1000, 8500, 1500);
-  animateValue(document.getElementById("number-4"), 10, 60, 1500);
+  animateValue(document.getElementById("number-1"), 100, 872, 1500);
+  animateValue(document.getElementById("number-2"), 1000, 1787, 1500);
+  animateValue(document.getElementById("number-3"), 1000, 10102, 1500);
+  animateValue(document.getElementById("number-4"), 10, 65, 1500);
   animated = true;
 }
 
